a15da56dc0ef3ba1ee4f9ff5e56364de2b03871b
hrloca <hr.loca@gmail.com>
2022-07-17 15:25:59 +0900

    fix: update typescript config and versions and more.

--- src/Grabbed.ts
+++ src/Grabbed.ts
@@ -8,73 +8,73 @@ export interface GrabbedOption {
 
 export class Grabbed {
   // mutable
-  private startPoint: Point | null = null
-  private grabbedPoint: Point | null = null
-  private offset: Point | null = null
+  #startPoint: Point | null = null
+  #grabbedPoint: Point | null = null
+  #offset: Point | null = null
 
-  private isOverGrabRange = false
-  private onGrabHandler?: (offset: Point) => void
-  private onGrabMoveHandler?: (offset: Point) => void
-  private onGrabReleaseHandler?: (offset: Point) => void
+  #isOverGrabRange = false
+  #onGrabHandler?: (offset: Point) => void
+  #onGrabMoveHandler?: (offset: Point) => void
+  #onGrabReleaseHandler?: (offset: Point) => void
 
   constructor(opt: GrabbedOption = {}) {
-    this.onGrabHandler = opt.onGrab
-    this.onGrabMoveHandler = opt.onGrabMove
-    this.onGrabReleaseHandler = opt.onGrabRelease
+    this.#onGrabHandler = opt.onGrab
+    this.#onGrabMoveHandler = opt.onGrabMove
+    this.#onGrabReleaseHandler = opt.onGrabRelease
 
-    this.init()
+    this.#init()
   }
 
   set onGrabbed(handler: (offset: Point) => void) {
-    this.onGrabHandler = handler
+    this.#onGrabHandler = handler
   }
 
   set onGrabMove(handler: (offset: Point) => void) {
-    this.onGrabMoveHandler = handler
+    this.#onGrabMoveHandler = handler
   }
 
   set onGrabRelease(handler: (offset: Point) => void) {
-    this.onGrabReleaseHandler = handler
+    this.#onGrabReleaseHandler = handler
   }
 
-  private init() {
-    this.startPoint = null
-    this.grabbedPoint = null
-    this.offset = null
-    this.isOverGrabRange = false
+  #init(): void {
+    this.#startPoint = null
+    this.#grabbedPoint = null
+    this.#offset = null
+    this.#isOverGrabRange = false
   }
 
   start(fromTheReferencePoint: Point): void {
-    this.init()
-    this.startPoint = fromTheReferencePoint
+    this.#init()
+    this.#startPoint = fromTheReferencePoint
   }
 
   move(point: Point, threshold: number): void {
-    if (!this.startPoint) return
-    const offset = point.sub(this.startPoint)
+    if (!this.#startPoint) return
+    const offset = point.sub(this.#startPoint)
 
     if (offset.distance >= threshold) {
-      if (!this.isOverGrabRange) {
-        this.isOverGrabRange = true
-        this.grabbedPoint = offset
-        this.onGrabHandler?.(offset)
+      if (!this.#isOverGrabRange) {
+        this.#isOverGrabRange = true
+        this.#grabbedPoint = offset
+        this.#onGrabHandler?.(offset)
       }
     }
 
-    if (this.isOverGrabRange) {
+    if (this.#isOverGrabRange) {
       // NOTE: grabbedPoint is confirmed.
-      this.offset = offset
-      this.onGrabMoveHandler?.(offset.sub(this.grabbedPoint as Point))
+      this.#offset = offset
+      this.#onGrabMoveHandler?.(offset.sub(this.#grabbedPoint as Point))
     }
   }
 
   end(): void {
-    if (!this.startPoint) return
+    if (!this.#startPoint) return
 
-    if (this.isOverGrabRange && this.offset) {
-      this.onGrabReleaseHandler?.(this.offset)
+    if (this.#isOverGrabRange && this.#offset) {
+      this.#onGrabReleaseHandler?.(this.#offset)
     }
 
-    this.init()
+    this.#init()
   }
 }
