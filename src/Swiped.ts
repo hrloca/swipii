8f6006779982d8c70b93607a258c6f7eb002565e
hrloca <hr.loca@gmail.com>
2022-07-17 15:54:17 +0900

    fix: to hard private

--- src/Swiped.ts
+++ src/Swiped.ts
@@ -18,37 +18,37 @@ export interface SwipedOption {
 }
 
 export class Swiped {
-  private readonly ANGLE_THREHOLD: number = 45
-  private readonly SWIPE_BORDER_THRESHOLD: number = 18
-  private readonly RELEASE_THRESHOLD: number = 3
-  private readonly GRABBED_THRESHOLD: number = 5
+  readonly #ANGLE_THREHOLD: number = 45
+  readonly #SWIPE_BORDER_THRESHOLD: number = 18
+  readonly #RELEASE_THRESHOLD: number = 3
+  readonly #GRABBED_THRESHOLD: number = 5
 
-  private useX = true
-  private useY = false
+  #useX = true
+  #useY = false
 
-  private maxValue = 0
-  private willEmitSwipe = false
-  private swipeable = false
-  private currentAxis: Axis | 'unknown' = 'unknown'
+  #maxValue = 0
+  #willEmitSwipe = false
+  #swipeable = false
+  #currentAxis: Axis | 'unknown' = 'unknown'
 
   // point state
-  private offset: Point = new Point()
+  #offset: Point = new Point()
 
   // handler
-  private onGrabbedHandler?: () => void
-  private onGrabbedMoveHandler?: (e: EventData) => void
-  private onGrabbedReleaseHandler?: (e: EventData) => void
-  private onSwipedHandler?: (e: EventData) => void
-  private onSwipedNotHandler?: (e: EventData) => void
+  #onGrabbedHandler?: () => void
+  #onGrabbedMoveHandler?: (e: EventData) => void
+  #onGrabbedReleaseHandler?: (e: EventData) => void
+  #onSwipedHandler?: (e: EventData) => void
+  #onSwipedNotHandler?: (e: EventData) => void
 
   private grabbed: Grabbed
 
   constructor(opt: SwipedOption = {}) {
-    this.onGrabbedHandler = opt.onGrabbed
-    this.onGrabbedMoveHandler = opt.onGrabbedMove
-    this.onGrabbedReleaseHandler = opt.onGrabbedRelease
-    this.onSwipedHandler = opt.onSwiped
-    this.onSwipedNotHandler = opt.onSwipedNot
+    this.#onGrabbedHandler = opt.onGrabbed
+    this.#onGrabbedMoveHandler = opt.onGrabbedMove
+    this.#onGrabbedReleaseHandler = opt.onGrabbedRelease
+    this.#onSwipedHandler = opt.onSwiped
+    this.#onSwipedNotHandler = opt.onSwipedNot
 
     this.resetState()
     this.grabbed = new Grabbed({
@@ -65,19 +65,19 @@ export class Swiped {
   }
 
   set onGrabbed(handle: (() => void) | undefined) {
-    this.onGrabbedHandler = handle
+    this.#onGrabbedHandler = handle
   }
   set onGrabbedMove(handle: ((e: EventData) => void) | undefined) {
-    this.onGrabbedMoveHandler = handle
+    this.#onGrabbedMoveHandler = handle
   }
   set onGrabbedRelease(handle: ((e: EventData) => void) | undefined) {
-    this.onGrabbedReleaseHandler = handle
+    this.#onGrabbedReleaseHandler = handle
   }
   set onSwiped(handle: ((e: EventData) => void) | undefined) {
-    this.onSwipedHandler = handle
+    this.#onSwipedHandler = handle
   }
   set onSwipedNot(handle: ((e: EventData) => void) | undefined) {
-    this.onSwipedNotHandler = handle
+    this.#onSwipedNotHandler = handle
   }
 
   start(fromTheReferencePoint: Point): void {
@@ -86,8 +86,8 @@ export class Swiped {
   }
 
   grabbing(point: Point, threshold?: number): boolean {
-    this.grabbed.move(point, threshold ?? this.GRABBED_THRESHOLD)
-    return this.swipeable
+    this.grabbed.move(point, threshold ?? this.#GRABBED_THRESHOLD)
+    return this.#swipeable
   }
 
   release(): void {
@@ -96,67 +96,67 @@ export class Swiped {
   }
 
   private resetState() {
-    this.offset = new Point()
-    this.willEmitSwipe = false
-    this.currentAxis = 'unknown'
-    this.swipeable = false
-    this.maxValue = 0
+    this.#offset = new Point()
+    this.#willEmitSwipe = false
+    this.#currentAxis = 'unknown'
+    this.#swipeable = false
+    this.#maxValue = 0
   }
 
   private maybeGrabbed(offset: Point): void {
     const { angle } = offset
-    const swipableX = this.useX && angle < 90 - this.ANGLE_THREHOLD
-    const swipableY = this.useY && angle > this.ANGLE_THREHOLD
+    const swipableX = this.#useX && angle < 90 - this.#ANGLE_THREHOLD
+    const swipableY = this.#useY && angle > this.#ANGLE_THREHOLD
 
-    this.swipeable = swipableX || swipableY
+    this.#swipeable = swipableX || swipableY
     // swipe
-    if (this.swipeable) {
-      this.currentAxis = swipableX ? 'x' : 'y'
-      this.onGrabbedHandler?.()
+    if (this.#swipeable) {
+      this.#currentAxis = swipableX ? 'x' : 'y'
+      this.#onGrabbedHandler?.()
     }
   }
 
   private grabMove(offset: Point) {
-    if (this.swipeable) {
-      if (this.currentAxis === 'unknown') return
+    if (this.#swipeable) {
+      if (this.#currentAxis === 'unknown') return
 
-      const prevPointAbs = this.offset.abs
+      const prevPointAbs = this.#offset.abs
       const currentPointAbs = offset.abs
       const diff = currentPointAbs.sub(prevPointAbs)
 
-      const axis = this.currentAxis
+      const axis = this.#currentAxis
       const currentValue = currentPointAbs[axis]
       const currentDiff = diff[axis]
 
       const shouldIncrease = currentDiff > 0
-      if (shouldIncrease) this.maxValue = currentValue
+      if (shouldIncrease) this.#maxValue = currentValue
 
-      const isOverSwipeableLimit = this.SWIPE_BORDER_THRESHOLD < currentValue
-      const withinRange = this.maxValue < currentValue + this.RELEASE_THRESHOLD
-      this.willEmitSwipe = isOverSwipeableLimit && withinRange
-      this.offset = offset
+      const isOverSwipeableLimit = this.#SWIPE_BORDER_THRESHOLD < currentValue
+      const withinRange = this.#maxValue < currentValue + this.#RELEASE_THRESHOLD
+      this.#willEmitSwipe = isOverSwipeableLimit && withinRange
+      this.#offset = offset
 
-      this.onGrabbedMoveHandler?.({
-        offset: this.offset,
-        axis: this.currentAxis,
-        willEmitSwipe: this.willEmitSwipe,
+      this.#onGrabbedMoveHandler?.({
+        offset: this.#offset,
+        axis: this.#currentAxis,
+        willEmitSwipe: this.#willEmitSwipe,
       })
     }
   }
 
   private grabRelease() {
-    if (this.swipeable) {
+    if (this.#swipeable) {
       const eventData = {
-        offset: this.offset,
-        axis: this.currentAxis,
-        willEmitSwipe: this.willEmitSwipe,
+        offset: this.#offset,
+        axis: this.#currentAxis,
+        willEmitSwipe: this.#willEmitSwipe,
       }
 
-      this.onGrabbedReleaseHandler?.(eventData)
-      if (this.willEmitSwipe) {
-        this.onSwipedHandler?.(eventData)
+      this.#onGrabbedReleaseHandler?.(eventData)
+      if (this.#willEmitSwipe) {
+        this.#onSwipedHandler?.(eventData)
       } else {
-        this.onSwipedNotHandler?.(eventData)
+        this.#onSwipedNotHandler?.(eventData)
       }
     }
   }
